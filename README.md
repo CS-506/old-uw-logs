cf6c11f34dd741f183515d08958783ca0434c598
thekeenant <keenan@keenant.com>
2017-11-16 01:11:23 -0600

    Adds parsing grade distributions, sis term code util

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 Making sense of UW Madison course data.
 
+## Parsing
+
+UW Madison publishes most of the data used in the project in the form of PDFs. We use
+[tabula-java](https://github.com/tabulapdf/tabula-java) to extract the data.
+
 ## Links
 
 * [UW Madison Facilities](https://fpm-www1.fpm.wisc.edu/smoasp/FacilityName.asp?OrderBy=Name)
 * [DIR Reports](https://registrar.wisc.edu/current-reports/)
 * [GPA Reports](https://registrar.wisc.edu/grade-reports/)
 * [SIS Term Codes](http://www.bussvc.wisc.edu/bursar/termcode.html)
+* [SIS Program Codes](https://registrar.wisc.edu/wp-content/uploads/sites/36/2017/06/list-sis-program-codes.pdf)?
---
+++ parsing/dir_parser.py
@@ -0,0 +1,44 @@
+import subprocess
+
+COLUMN_DIVIDERS = ','.join(map(str, [
+  55,80,95,130,150,210,295,360,420,480,550
+]))
+
+process = subprocess.Popen([
+  'java', 
+  '-jar', 
+  'tabula-1.0.1-jar-with-dependencies.jar', 
+  '1182-Final-DIR-Report.pdf',
+  '--columns=' + COLUMN_DIVIDERS,
+  '--format=TSV',
+  '--pages=all'
+], stdout=subprocess.PIPE)
+
+out, err = process.communicate()
+
+for row in out.split("\r\n"):
+  cols = row.split("\t")
+
+  # valid rows have 12 columns
+  if len(cols) != 12:
+    continue
+
+  # valid rows should have a course number
+  # if it does not parse, we skip this row
+  try:
+    int(cols[1])
+  except ValueError:
+    continue
+  
+  # unknown values: cols[0], cols[4]
+
+  course_num = int(cols[1])
+  section_type = cols[2]
+  section_num = cols[3]
+  time = cols[5]
+  days = cols[6]
+  facility = cols[7]
+  combined_enrollment = cols[8] # combines cross-listed courses
+  total_enrollment = cols[9]
+  instructor_id = cols[10]
+  instructor = cols[11]
