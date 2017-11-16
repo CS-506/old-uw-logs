9d0748068630bbd3ed38b5d720263450c2f7d8d5
thekeenant <keenan@keenant.com>
2017-11-15 21:06:50 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# uw-logs
+
+Making sense of UW Madison course data.
+
+## Links
+
+* [UW Madison Facilities](https://fpm-www1.fpm.wisc.edu/smoasp/FacilityName.asp?OrderBy=Name)
+* [DIR Reports](https://registrar.wisc.edu/current-reports/)
+* [GPA Reports](https://registrar.wisc.edu/grade-reports/)
---
+++ backend/dir_parser.py
@@ -0,0 +1,39 @@
+import subprocess
+
+process = subprocess.Popen([
+  'java', 
+  '-jar', 
+  'tabula-1.0.1-jar-with-dependencies.jar', 
+  '1182-Final-DIR-Report.pdf',
+  '--columns=55,80,95,130,150,210,295,360,420,480,550',
+  '--format=TSV',
+  '--pages=all'
+], stdout=subprocess.PIPE)
+
+out, err = process.communicate()
+
+for row in out.split("\r\n"):
+  cols = row.split("\t")
+
+  if len(cols) < 12:
+    continue
+
+  # valid rows should have a course number
+  # if it does not parse, we skip this row
+  try:
+    int(cols[1])
+  except ValueError:
+    continue
+  
+  # unknown values: cols[0], cols[4]
+
+  course_number = int(cols[1])
+  section_type = cols[2]
+  section_number = cols[3]
+  time = cols[5]
+  days = cols[6]
+  facility = cols[7]
+  combined_enrollment = cols[8] # combines cross-listed courses
+  total_enrollment = cols[9]
+  instructor_id = cols[10]
+  instructor = cols[11]
